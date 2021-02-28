c446395d3c5a93d3bf22ba828456ffa2b9a72fdd
2langk <leelk980@gmail.com>
2021-02-28 14:11:43 +0900

    eslint config + body parser

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,8 @@ process.on('uncaughtException', (err) => {
 const app = express();
 
 app.enable('trust proxy');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
 app.use(compression());
